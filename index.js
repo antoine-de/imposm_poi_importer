a1b3ece1e679ca87db247248112d4af9210f812a
antoine-de <antoine.desbordes@gmail.com>
2017-12-20 17:02:37 +0100

    es lint

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-'use strict'
-
 const { Pool } = require('pg');
 const _ = require('lodash');
-const Document = require('pelias-model').Document;
+const { Document } = require('pelias-model');
 const http = require('tiny-json-http');
 const elasticsearch = require('elasticsearch');
 const yargs = require('yargs');
@@ -10,7 +8,7 @@ const yargs = require('yargs');
 const LAYER = 'venue';
 const SOURCE = 'openstreetmap';
 
-async function get_all_pois(pg) {
+async function getAllPois(pg) {
   const query = `select id,
     st_x(st_transform(geometry, 4326)) as lon,
     st_y(st_transform(geometry, 4326)) as lat,
@@ -19,22 +17,22 @@ async function get_all_pois(pg) {
 
   const pool = new Pool({
     connectionString: pg,
-  })
+  });
 
-  console.log(`querying poi database`);
+  console.log('querying poi database');
   const res = await pool.query(query);
 
   await pool.end();
   return res.rows;
 }
 
-function create_pelias_document(p) {
+function createPeliasDocument(p) {
   const doc = new Document(SOURCE, LAYER, p.id);
 
   doc.setName('default', _.trim(p.name));
   doc.setCentroid({
     lat: p.lat,
-    lon: p.lon
+    lon: p.lon,
   });
   if (!_.isEmpty(_.trim(p.house_number))) {
     doc.setAddress('number', _.trim(p.house_number));
@@ -49,71 +47,72 @@ function create_pelias_document(p) {
   return doc;
 }
 
-async function locate(pelias_poi, pip) {
-  const url = `${pip}/${pelias_poi.center_point.lon}/${pelias_poi.center_point.lat}`;
+async function locate(peliasPoi, pip) {
+  const url = `${pip}/${peliasPoi.center_point.lon}/${peliasPoi.center_point.lat}`;
   try {
     const res = await http.get({ url });
 
     Object.entries(res.body).forEach(([layer, admin]) => {
-      pelias_poi.addParent(
+      peliasPoi.addParent(
         layer,
         admin[0].name,
         admin[0].id.toString(),
-        admin[0].abbr);
+        admin[0].abbr,
+      );
     });
-    return pelias_poi;
   } catch (e) {
-    setImmediate(() => { throw e });
+    setImmediate(() => { throw e; });
   }
+  return peliasPoi;
 }
 
-function convert_to_pelias(pois, pip) {
-  return pois.map(poi => {
-    const doc = create_pelias_document(poi);
+function convertToPelias(pois, pip) {
+  return pois.map((poi) => {
+    const doc = createPeliasDocument(poi);
     return locate(doc, pip);
   });
 }
 
-function send_to_es(pelias_pois, es_host) {
+function sendToEs(peliasPois, esHost) {
   const client = new elasticsearch.Client({
-    host: es_host,
-    log: 'info'
+    host: esHost,
+    log: 'info',
   });
 
-  var nb_errors = 0,
-    nb_attempt = 0;
+  let nbErrors = 0;
+  let nbAttempt = 0;
 
-  pelias_pois.forEach(poi => {
-    nb_attempt++;
+  peliasPois.forEach((poi) => {
+    nbAttempt += 1;
     poi.then(p => p.toESDocument().data)
-      .then(async doc => {
-        console.log("document send to es: ", JSON.stringify(doc, null, 4));
+      .then(async (doc) => {
+        console.log('document send to es: ', JSON.stringify(doc, null, 4));
         return client.index({
           index: 'pelias',
           type: 'venue',
-          id: 'imposm:' + doc.source_id,
-          body: doc
+          id: `imposm:${doc.source_id}`,
+          body: doc,
         });
       })
-      .catch(e => {
-        console.error("impossible to insert doc ", poi, "because ", e);
-        nb_errors++;
+      .catch((e) => {
+        console.error('impossible to insert doc ', poi, 'because ', e);
+        nbErrors += 1;
       });
 
-    if (nb_errors > 0) {
-      console.error(`${nb_errors} errors /${nb_attempt} documents`)
+    if (nbErrors > 0) {
+      console.error(`${nbErrors} errors /${nbAttempt} documents`);
     }
   });
 }
 
-function import_poi_in_pelias(pg, es, pip) {
-  get_all_pois(pg)
-    .then(pois => convert_to_pelias(pois, pip))
-    .then(pelias_pois => send_to_es(pelias_pois, es))
-    .catch(e => setImmediate(() => { throw e }))
+function importPoiInPelias(pg, es, pip) {
+  getAllPois(pg)
+    .then(pois => convertToPelias(pois, pip))
+    .then(peliasPois => sendToEs(peliasPois, es))
+    .catch(e => setImmediate(() => { throw e; }));
 }
 
-let args = yargs
+const args = yargs
   .usage('Usage: $0 [options]')
   .options({
     pg: {
@@ -121,8 +120,8 @@ let args = yargs
       type: 'string',
       demandOption: true,
       default: 'postgres://gis:gis@localhost/gis',
-      nargs: 1
-    }
+      nargs: 1,
+    },
   })
   .options({
     es: {
@@ -130,8 +129,8 @@ let args = yargs
       type: 'string',
       demandOption: true,
       default: 'localhost:9200',
-      nargs: 1
-    }
+      nargs: 1,
+    },
   })
   .options({
     pip: {
@@ -139,11 +138,11 @@ let args = yargs
       type: 'string',
       demandOption: true,
       default: 'http://localhost:4200',
-      nargs: 1
-    }
+      nargs: 1,
+    },
   })
   .help('h')
   .alias('h', 'help')
   .argv;
 
-import_poi_in_pelias(args.pg, args.es, args.pip);
+importPoiInPelias(args.pg, args.es, args.pip);
